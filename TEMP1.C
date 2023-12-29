4ff291b6841c27df36f0acc08f1034c0935f781d
4072-jenish <150036720+4072-jenish@users.noreply.github.com>
2023-12-29 15:49:19 +0530

    Update TEMP1.C

--- TEMP1.C
+++ TEMP1.C
@@ -1,3 +1,11 @@
+
+/*
+Q.1 Write a Program to print all alphabets from a to z by skipping 3 alphabets using do while loop.
+For example,
+Output: 
+a, e, i, m, q, u, y
+*/
+
 #include<stdio.h>
 #include<conio.h>
 
